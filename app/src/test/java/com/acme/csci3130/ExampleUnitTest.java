3f9a409fae436ff0d74730da2a23eb8d2556cfcf
caleb2004 <Caleb@HANNINGdeMacBook-Pro.local>
2017-05-30 02:22:21 -0300

    test

--- app/src/test/java/com/acme/csci3130/ExampleUnitTest.java
+++ app/src/test/java/com/acme/csci3130/ExampleUnitTest.java
@@ -10,8 +10,34 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
+    @Test//
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+
+    @Test
+public void IntegerTest() throws Exception{
+        int a = 2, b = 2;
+        assertEquals(a,b);
+    }
+
+    @Test
+    public void IntegerTest2() throws Exception{
+        int x = 1, y = 2;
+        assertFalse(x == y);
+    }
+
+    @Test
+    public void arrayTest() throws Exception{
+        int []a = new int[100];
+        for(int i=0;i<100;i++){
+            a[i] = i;
+        }
+        int []b = new int[100];
+        for(int i=0;i<100;i++){
+            b[i] = i;
+        }
+        assertArrayEquals(a,b);
     }
 }
